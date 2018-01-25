ca154020da7b84884a0ad4d9add916b03a42a175
Daniel L.Branco <danlb48986@gmail.com>
2018-01-24 20:12:43 -0600

    create the Controller class, and the home controller.

---
+++ classes/Controller.php
@@ -0,0 +1,28 @@
+<?php
+//Controller class - base controller to create other controllers.
+abstract class Controller{
+	//Class variables. 
+	protected $request;
+	protected $action;
+
+	//Constructor method.
+	public function __construct($action, $request){
+		$this->action = $action;
+		$this->request = $request;
+	}//Ends constructor.
+
+	public function executeAction(){
+		return $this->{$this->action}();
+	}//Ends executeAction.
+
+	protected function returnView($viewmodel, $fullview){
+		$view = "views/{$get_class($this)}/".$this->action.".php";
+
+		if($fullview){
+			require("views/main.php");
+		} else {
+			require("view");
+		}
+	}//Ends returnView.
+}//Ends Controller class.
+?>
--- index.php
+++ index.php
@@ -2,9 +2,14 @@
 //Inlcude some php files.
 require("config.php");
 require("classes/Bootstrap.php");
-
+require("classes/Controller.php");
+require("controllers/home.php");
 
 //Create an instance of the Bootstrap class.
 $bootstrap = new Bootstrap($_GET);
 $controller = $bootstrap->createController();
+
+if($controller){
+	$controller->executeAction();
+}
 ?>
