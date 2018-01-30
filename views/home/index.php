b3052b5552ac159329214b38c7d080c937103478
Daniel L.Branco <danlb48986@gmail.com>
2018-01-30 01:02:31 -0600

    updating style

--- views/home/index.php
+++ views/home/index.php
@@ -2,6 +2,6 @@
 	<h1>Welcome to my Shareboard</h1>
 	<p class="lead">Did you read or have seen something interesting currently? 
 	<br/>Share it with your friends, family and community.</p>
-	<a class="btn btn-primary text-center" href="<?php echo ROOT_URL; ?>posts">
+	<a id="share_button" class="btn btn-primary text-center" href="<?php echo ROOT_URL; ?>posts">
 	Share it</a>
 </div>
--- views/main.php
+++ views/main.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html>
 	<head>
-		<title>Shareboard</title>
+		<title>Create a site with MVC Shareboard</title>
 
 		<!-- Bootstrap core CSS -->
 		<link rel="stylesheet" type="text/css" href="<?php echo ROOT_URL; ?>assets/css/bootstrap.css">
@@ -13,13 +13,12 @@
 		<nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
 			<div class="container">
 		        <a class="navbar-brand" href="<?php echo ROOT_URL; ?>">MVC Shareboard</a>
-		        <button class="navbar-toggler" type="button" data-toggle="collapse" 
-		        data-target="#navbar" aria-controls="navbar" aria-expanded="false" 
-		        aria-label="Toggle navigation">
-		        	<span class="navbar-toggler-icon"></span>
-	        	</button>
 
-		        <div id="navbar" class="collapse navbar-collapse">
+	        	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+	        		<span class="navbar-toggler-icon"></span>
+		   		 </button>
+
+		        <div id="navbarCollapse" class="collapse navbar-collapse">
 					<ul class="navbar-nav mr-auto">
 						<li class="nav-item">
 							<a class="nav-link" href="<?php echo ROOT_URL; ?>">
@@ -64,5 +63,9 @@
 	    	</div>
     	</div><!-- ends container-->
 
+    	<!-- Javascript links -->
+    	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
   	</body>
 </html>
